fc10af1fc5d22a034901ecc7324731e58dfd3231
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-01 07:30:53 +0000

    feat: First commit

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    todos_json = jsonify(todos)
+    return todos_json
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json()
+    todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.remove((todos[position-1]))
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
+    
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
